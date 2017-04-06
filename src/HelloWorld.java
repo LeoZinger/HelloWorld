b12676802e7f4b0abf5b21fcf81d243babdb1ba2
LeoZinger <leo.zinger@gmail.com>
2017-04-05 22:17:42 -0400

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * Created by leo.zinger on 3/15/17.
+ */
+public class HelloWorld {
+
+    final static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
